ef2fa8da84e0e0de094c44bd79abd13b3cf337fc
Jerzy Tymofiejczyk <128184858+JurekWT@users.noreply.github.com>
2023-10-10 09:37:03 +0200

    Update Program.cs

--- Rozdział 2/Zadanie 2.1/Program.cs
+++ Rozdział 2/Zadanie 2.1/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Podaj temperaturę w stopniach celsjusza:");
-float c = float.Parse(Console.ReadLine());
-float f = 32 + ((9 * c) / 5);
+double c = double.Parse(Console.ReadLine());
+double f = 32 + (1.8 * c);
 Console.WriteLine("W Fahrenheitach to: {0}", f);
---
+++ Wariant 16 - wejsciowka/Wariant 16 - wejsciowka/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Podaj kwotę netto:");
+double netto = double.Parse(Console.ReadLine());
+Console.WriteLine("Podaj stawkę podatku w procentach:");
+double podatek = double.Parse(Console.ReadLine());
+double brutto = netto * (1.00 + (podatek/100));
+Console.WriteLine("Kwota brutto wynosi {0}", brutto);
