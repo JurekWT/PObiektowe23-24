ba6d12aba6ac514e192ff9942c8038fa5c732ed4
Jerzy Tymofiejczyk <128184858+JurekWT@users.noreply.github.com>
2023-10-22 14:38:40 +0200

    zadanka

---
+++ Rozdział 3/Zadanie 3-1/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Podaj rok:");
+int year = int.Parse(Console.ReadLine());
+if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+    Console.WriteLine("{0} to rok przestępny.", year);
+else
+    Console.WriteLine("{0} nie jest rokiem przestępnym.", year);
---
+++ Rozdział 3/Zadanie 3-2/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Podaj pierwszą liczbę całkowitą:");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Podaj drugą liczbę całkowitą:");
+int b = int.Parse(Console.ReadLine());
+if (a % b == 0)
+    Console.WriteLine("Liczba {0} jest dzielnikiem liczby {1}", b,a);
+    else
+    Console.WriteLine("Liczba {0} nie jest dzielnikiem liczby {1}", b,a);
