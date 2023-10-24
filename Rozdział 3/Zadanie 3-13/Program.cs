f8ddd3caa29a7602bf1657d57bc701eb22ff6166
Jerzy Tymofiejczyk <128184858+JurekWT@users.noreply.github.com>
2023-10-24 20:55:50 +0200

    done

--- Rozdział 3/Zadanie 3-13/Program.cs
+++ Rozdział 3/Zadanie 3-13/Program.cs
@@ -4,8 +4,16 @@ Console.WriteLine("Dany jest szereg W(n)=1 – 2 + 3 – 4 + ...\u00b1 n");
 Console.WriteLine("Podaj n");
 int n = int.Parse(Console.ReadLine());
 int wynik = 0;
-while (n != 0)
+for (int i = 1; i < n+1; i++) 
 {
-    
+    if (i % 2== 0)
+    { 
+        wynik -= i;
+    }
+    else
+    {
+        wynik += i;
+    }
 }
+Console.WriteLine(wynik);
 
---
+++ Rozdział 3/Zadanie 3-14/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Znajdź liczby doskonałe w przedziale <1,n>. Podaj n");
+int n = int.Parse(Console.ReadLine());
+for (int i = 1; i < n+1; i++)
+{
+    int suma = 0;
+    for (int j = 1; j < i; j++)
+    {
+        if (i%j==0)
+        {
+            suma += j;
+        }
+    }
+
+    if (suma == i)
+    {
+        Console.Write("{0} ", suma);
+    }
+}
