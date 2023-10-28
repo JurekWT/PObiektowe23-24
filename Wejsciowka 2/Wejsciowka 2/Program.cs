29b3ab13685c40c00193f7843ceaddc6079a1d1f
Jerzy Tymofiejczyk <128184858+JurekWT@users.noreply.github.com>
2023-10-28 15:24:04 +0200

    pusz

---
+++ Wejsciowka 2/Wejsciowka 2/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Podaj dzień urodzin:");
+int x = int.Parse(Console.ReadLine());
+Console.WriteLine("Podaj miesiąc urodzin:");
+int y = int.Parse(Console.ReadLine());
+y += 55;
+for (int i = x; i < y+1; i++)
+{
+    if (i % 2 != 0)
+    {
+        Console.WriteLine(i);
+    }
+}
