48dd94917beb527c2a1134c8c7a5e61e2095511b
Jerzy Tymofiejczyk <128184858+JurekWT@users.noreply.github.com>
2023-11-15 20:06:44 +0100

    update

---
+++ Rozdzial 4/Zadanie 4-1/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Podaj liczbę elementów tablicy");
+int numele = int.Parse(Console.ReadLine());
+int[] tablica = new int[numele];
+for (int i = 0; i < numele; i++)
+{
+    Console.WriteLine("Podaj element nr {0}", i+1);
+    tablica[i] = int.Parse(Console.ReadLine());
+}
+Console.Write("Elementy w tablicy: ");
+for (int i = 0; i < numele; i++)
+{
+    Console.Write("{0} ", tablica[i]);
+}
