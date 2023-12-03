116d8bc0420fe234996b2e3f5ced7884208f4753
Jerzy Tymofiejczyk <128184858+JurekWT@users.noreply.github.com>
2023-12-03 12:53:32 +0100

    zajęcia

---
+++ Rozdział 6/Zadanie_6-4/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+internal class Program
+{
+    public class Energia
+    {
+        public int PoczatkowyStan { private set; get; }
+        public int BiezacyStan { set; get; }
+
+        public Energia(int pocz, int biez)
+        {
+            PoczatkowyStan = pocz;
+            BiezacyStan = biez;
+        }
+
+        public void PokazStanLicznika()
+        {
+            Console.WriteLine($"Stan początkowy licznika: {PoczatkowyStan} kWh, bieżący stan licznika: {BiezacyStan}kWh");
+        }
+
+        public void PokazZuzycieEnergii()
+        {
+            Console.WriteLine($"Zużycie energii to {BiezacyStan-PoczatkowyStan} kWh");
+        }
+    }
+    public static void Main(string[] args)
+    {
+        Energia abc = new Energia(200, 4000);
+        abc.PokazStanLicznika();
+        abc.PokazZuzycieEnergii();
+    }
+}
---
+++ Rozdział 6/Zadanie_6-5/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+internal class Program
+{
+    public class Punkt
+    {
+        public int WspA { get; set; }
+        public int WspB { get; set; }
+        public string NazwaPunktu { get; set; }
+
+        public Punkt(string nazwa, int osX, int osY)
+        {
+            this.WspA = osX;
+            this.WspB = osY;
+            this.NazwaPunktu = nazwa;
+        }
+
+        public void Przesun(int osX, int osY)
+        {
+            WspA += osX;
+            WspB += osY;
+        }
+
+        public void Wyswietl()
+        {
+            Console.WriteLine($"Aktualne współrzędne punktu {NazwaPunktu} to ({WspA},{WspB})");
+        }
+
+    }
+    public static void Main(string[] args)
+    {
+        Punkt a = new Punkt("A",4, 6);
+        a.Przesun(2,0);
+        a.Wyswietl();
+    }
+}
