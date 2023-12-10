2fcb51ed32c68bfc96b0b31a285aa403cbb02e8e
Jerzy Tymofiejczyk <128184858+JurekWT@users.noreply.github.com>
2023-12-10 11:06:54 +0100

    wejściówka

---
+++ Wejściówka 10-12-2023/Wejściówka 10-12-2023/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+internal class Program
+{
+    public class Trojkat
+    {
+        public double pierwszyBok;
+        public double drugiBok;
+        public double trzeciBok;
+
+        public string CzyTrojkat()
+        {
+            if (pierwszyBok > 0 && drugiBok > 0 && trzeciBok > 0)
+            {
+                if (pierwszyBok + drugiBok > trzeciBok && pierwszyBok + trzeciBok > drugiBok && drugiBok + trzeciBok > pierwszyBok)
+                {
+                    return "tak";
+                }
+                else
+                {
+                    return "nie";
+                }
+            }
+            else
+            {
+                return "nie";
+            }
+        }
+
+    }
+    public static void Main(string[] args)
+    {
+        Trojkat triangle1 = new Trojkat();
+        triangle1.pierwszyBok = 3.00;
+        triangle1.drugiBok = 2.00;
+        triangle1.trzeciBok = 4.00;
+        Console.WriteLine($"Trójkąt o wymiarach {triangle1.pierwszyBok}x{triangle1.drugiBok}x{triangle1.trzeciBok} istnieje? {triangle1.CzyTrojkat()}");
+    }
+}
