daa9daee6892a9d97567a3040efe049c48469ea7
Jerzy Tymofiejczyk <128184858+JurekWT@users.noreply.github.com>
2023-12-13 19:05:39 +0100

    upd

---
+++ Rozdział 6/Zadanie_6-8/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Zadanie_6_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Prostopadloscian first = new Prostopadloscian(20, 30, 40);
+            Prostopadloscian second = new Prostopadloscian(10, 40, 50);
+            Prostopadloscian.PorownajObjetosc(second, first);
+
+        }
+    }
+}
---
+++ Rozdział 6/Zadanie_6-8/Prostopadloscian.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_6_8
+{
+    internal class Prostopadloscian
+    {
+        public double dlugosc;
+        public double szerokosc;
+        public double wysokosc;
+
+        public Prostopadloscian(double d, double s, double w)
+        {
+            this.dlugosc = d;
+            this.szerokosc = s;
+            this.wysokosc = w;
+        }
+
+        public double Objetosc()
+        {
+            return dlugosc * szerokosc * wysokosc;
+        }
+
+        public static Prostopadloscian PorownajObjetosc(Prostopadloscian a, Prostopadloscian b)
+        {
+            if (a.Objetosc() > b.Objetosc())
+            {
+                return a;
+            }
+            else
+            {
+                return b;
+            }
+        }
+    }
+}
